a9654d516c69e8d52ea9d1bea99c9b3c8f4d4ef2
postfm <postfm@yandex.ru>
2023-09-14 15:38:47 +0300

    Добавит класс wrapper

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+    colors: {
+      'yellow': '#FFBD59',
+      'beige': '#BEA495',
+      'beige1': '#7B6152',
+      'beige2': '#A5978E',
+      'pink': '#FFF8F3',
+      'grey': '#FAFAFA',
+      'black': '#000000',
+      'white': '#FFFFFF'
+    },
     extend: {},
   },
   plugins: [],
